64285a16109d9de7934c56579ad73a7821d40fca
REN Hao <renhaojules@163.com>
2019-09-26 18:52:42 +0800

    新增爬取百度百科人物数据

---
+++ Hao Test/scrapt_baidu_person.py
@@ -0,0 +1,11 @@
+# 作者：hao.ren3
+# 时间：2019/9/26 11:25
+# IDE：PyCharm
+
+from tools.baidu_class import baidu_person
+
+baidu_class = baidu_person()
+baidu_class.get_person_web('李幼斌')
+baidu_class.get_person_web('武亦姝')
+baidu_class.get_person_web('徐锦江')
+baidu_class.get_person_web('袁隆平')
